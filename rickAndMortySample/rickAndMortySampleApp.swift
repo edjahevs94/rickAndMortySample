7d1933aad874fd1d2059147c30bc21c616366750
Edgard <20133252@ealoe.ulima.edu.pe>
2023-10-16 09:57:28 -0500

    Initial Commit

---
+++ rickAndMortySample/rickAndMortySampleApp.swift
@@ -0,0 +1,17 @@
+//
+//  rickAndMortySampleApp.swift
+//  rickAndMortySample
+//
+//  Created by EdgardVS on 16/10/23.
+//
+
+import SwiftUI
+
+@main
+struct rickAndMortySampleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
